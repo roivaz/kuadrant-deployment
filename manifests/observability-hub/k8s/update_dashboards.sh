a6cad8c294e8c762cab6df65dd60dcc5e3858810
David Martin <davmarti@redhat.com>
2024-10-31 14:50:22 +0000

    Update example dashboards
    Signed-off-by: David Martin <davmarti@redhat.com>

--- manifests/observability-hub/k8s/update_dashboards.sh
+++ manifests/observability-hub/k8s/update_dashboards.sh
@@ -10,5 +10,7 @@ mkdir -p "$DASHBOARD_DIR"
 curl -sL https://github.com/Kuadrant/kuadrant-operator/raw/main/examples/dashboards/app_developer.json -o "${DASHBOARD_DIR}/grafana-dashboard-app-developer.json"
 curl -sL https://github.com/Kuadrant/kuadrant-operator/raw/main/examples/dashboards/platform_engineer.json -o "${DASHBOARD_DIR}/grafana-dashboard-platform-eng.json"
 curl -sL https://github.com/Kuadrant/kuadrant-operator/raw/main/examples/dashboards/business_user.json -o "${DASHBOARD_DIR}/grafana-dashboard-business-user.json"
+curl -sL https://github.com/Kuadrant/kuadrant-operator/raw/main/examples/dashboards/controller-resources-metrics.json -o "${DASHBOARD_DIR}/grafana-dashboard-controller-resources-metrics.json"
+curl -sL https://github.com/Kuadrant/kuadrant-operator/raw/main/examples/dashboards/controller-runtime-metrics.json -o "${DASHBOARD_DIR}/grafana-dashboard-controller-runtime-metrics.json"
 
 echo "Dashboard files updated."
